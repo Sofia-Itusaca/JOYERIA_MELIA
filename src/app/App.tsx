914543362ce5c7d191bebf012b4e510eae1e5a07
Sofia Itusaca <ana.itusaca@unas.edu.pe>
2026-03-26 23:43:15 -0500

    fix router final

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { Toaster } from "./components/ui/sonner";
-import BottomNav from "./components/BottomNav";
 
 export default function App() {
-  return (
-    <>
-      <RouterProvider router={router} />
-      <Toaster position="top-right" />
-      <BottomNav />
-    </>
-  );
+  return <RouterProvider router={router} />
 }
