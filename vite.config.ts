e6cb752989f58cb11d355b7cde17525ad154eca1
Sofia Itusaca <ana.itusaca@unas.edu.pe>
2026-03-26 23:18:01 -0500

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/JOYERIA_MELIA/',
+  base: '/',
 
   plugins: [
     react(),
