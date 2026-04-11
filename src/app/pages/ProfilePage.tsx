5c2d73a91f34efb5239fd8ba66c01f74fc5edd86
Sofia Itusaca <ana.itusaca@unas.edu.pe>
2026-04-11 18:18:55 -0500

    x

--- src/app/pages/ProfilePage.tsx
+++ src/app/pages/ProfilePage.tsx
@@ -119,7 +119,7 @@ export function ProfilePage() {
           <TabsContent value="info">
             <Card>
               <CardHeader className="flex flex-row items-center justify-between">
-                <CardTitle>Información Personal</CardTitle>
+                <CardTitle>INFORMACIÓN PERSONAL</CardTitle>
 
                 <Button
                   className="bg-[#5b4c9f] hover:bg-[#4a3d85]"
@@ -233,7 +233,7 @@ export function ProfilePage() {
           <TabsContent value="address">
             <Card>
               <CardHeader className="flex flex-row items-center justify-between">
-                <CardTitle>Dirección</CardTitle>
+                <CardTitle>DIRECCIÓN</CardTitle>
 
                 <Button
                 className="bg-[#5b4c9f] hover:bg-[#4a3d85]"
